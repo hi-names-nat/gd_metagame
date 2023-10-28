7395d20b9a5bd829650f3ebe065bdbac600e7494
natalie <natalie.soltis@outlook.com>
2023-10-28 01:07:49 -0400

    add hacky but functional version of clion compatability. need to debug debug.

---
+++ src/gdexample.cpp
@@ -0,0 +1,8 @@
+//
+// Created by natalie on 10/28/23.
+//
+
+#include "gdexample.h"
+namespace godot {
+
+}
---
+++ src/gdexample.h
@@ -0,0 +1,14 @@
+﻿#ifndef GD_METAGAME_GDEXAMPLE_H
+#define GD_METAGAME_GDEXAMPLE_H
+
+#include <godot_cpp/classes/sprite2d.hpp>
+namespace godot {
+    class GDExample: public Sprite2D {
+        GDCLASS(GDExample, Sprite2D)
+
+    private:
+        float time_passed;
+    };
+}
+
+#endif //GD_METAGAME_GDEXAMPLE_H
