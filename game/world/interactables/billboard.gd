6d4648a36f17852ed7c73caf10c4ca0540c04dbc
Natalie <natalie.soltis@outlook.com>
2025-01-07 21:08:53 -0500

    add models

--- game/world/interactables/billboard.gd
+++ game/world/interactables/billboard.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation = get_viewport().get_camera_3d().global_rotation
+	global_rotation = get_viewport().get_camera_3d().global_rotation
--- game/world/interactables/g_use_component.gd
+++ game/world/interactables/g_use_component.gd
@@ -2,17 +2,18 @@ extends CollisionObject3D
 
 signal OnInteracted
 
-@export var Outline: Node3D
+@export var Outlines: Array[Node3D]
 
 func use_object() -> void:
-	print("beb!!!")
 	OnInteracted.emit()
 	for ch in get_parent().get_children():
 		if ch.has_method("object_interacted"):
 			ch.object_interacted()
 
 func player_lookat() -> void:
-	Outline.visible = true
+	for o in Outlines:
+		o.visible = true
 
 func player_stop_look() -> void:
-	Outline.visible = false
+	for o in Outlines:
+		o.visible = false
